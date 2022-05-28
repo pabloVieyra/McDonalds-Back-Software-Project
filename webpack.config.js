27c30d1969223dcdbef90ee9d958ba180084563b
Julian Kominovic <juliankominovic@gmail.com>
2022-05-28 11:55:08 -0300

    #FIX: webpack target

--- webpack.config.js
+++ webpack.config.js
@@ -3,18 +3,16 @@ const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
   entry: "./src/index.js",
+  target: "node",
   output: {
     filename: "index.js",
     path: path.resolve(__dirname, "dist"),
   },
   resolve: {
-    modules: ["node_modules"],
     extensions: [".js"],
+    modules: [path.join(__dirname, "node_modules")],
+    extensions: [".js"],
+    symlinks: false,
   },
-  externals: [
-    nodeExternals({
-      modulesDir: path.resolve(__dirname, "./node_modules"),
-    }),
-    nodeExternals(),
-  ],
+  externals: [nodeExternals()],
 };
